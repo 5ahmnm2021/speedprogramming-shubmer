a610622ce5c9e1f6ed374dbec997d27d52066199
steffikh <stefanie.hubmer@htl-salzburg.ac.at>
2021-04-13 09:29:46 +0200

    Script fuer Screen 1 fertig

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Controller : MonoBehaviour
+{
+    public Button falsch1;
+    public Button falsch2;
+    public Button richtig;
+    public GameObject falschtext;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        falschtext.SetActive(false);
+
+        falsch1.GetComponent<Button>();
+        falsch1.onClick.AddListener(OnClick);
+
+        falsch2.GetComponent<Button>();
+        falsch2.onClick.AddListener(OnClick);
+    }
+
+    void OnClick()
+    {
+        Debug.Log("Falsche Antwort");
+        falschtext.SetActive(true);
+
+    }
+
+    public void ChangeScene1()
+    {
+        SceneManager.LoadScene("Screen2");
+    }
+}
